cda3d7f6a4d7471ea65257e7f6f1bf67f0b71797
Shirdi Nagaraju Mudunuri <Mudunuri@civicplus.com>
2019-09-30 11:38:50 -0500

    Added Explanation

--- src/leetCode/PeakIndex.java
+++ src/leetCode/PeakIndex.java
@@ -3,6 +3,19 @@ package leetCode;
 public class PeakIndex {
 
 	public static void main(String[] args) {
+		/*
+		 * Explanation:
+		 * Take two indices left and right
+		 * Take mid index using the left and right index
+		 * Now loop through the array until left is less than right
+		 * In every loop check if mid value is less than the mid+1 value
+		 * 	- We are checking this because if mid value is less than 
+		 * 	  mid +1 value, that means we are still in the ascending order of the array
+		 * 	  so we should move our left index to the mid +1 and do the check again
+		 * If mid value is greater than mid +1 value then we move our right index to the mid value
+		 * 	- Since we have our mid greater than mid +1 we need to move our right index
+		 * Out final left or right index will contain the same index logically and we return left or right.
+		 */
 		
 		int[] input = {0,2,1,0};
 		
