909431227d0d28e13037844b9e5b958e1a785e78
Raj <rajmudunuri1217@gmail.com>
2019-10-15 15:55:14 -0500

    updates to explanation

--- src/leetCode/PalindromeLinkedList.java
+++ src/leetCode/PalindromeLinkedList.java
@@ -49,6 +49,22 @@ public class PalindromeLinkedList {
 	}
 
 	private static Boolean isPalindrome(ListNode NodeHead) {
+		/*
+		 * If we reverse the given head straight away we will end up modifying the head and
+		 * we will only have the final node in our current list
+		 * so we need to split the lined list into two parts
+		 * for that we take two nodes fast and slow
+		 * fast will move one step faster than the slow both starting at the head node
+		 * we move fast node until fast or fast.next is null
+		 * we will end up with fast at end of the node and slow at mid node
+		 * now set the fast to head of the node
+		 * reverse the slow node and that will reverse the second part of the node
+		 * after reversal we will traverse slow and fast at the same pace
+		 * and compare values for each
+		 * if we find a value that's not matching we will return false
+		 * after the loop if we still didn't return anything means we have a palindrome and we return ture
+		 * 
+		 */
 		ListNode slow = NodeHead;
 		ListNode fast = NodeHead;
 		while(fast!=null && fast.next!=null) {
